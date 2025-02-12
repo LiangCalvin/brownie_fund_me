cb2f0919f47bb6590d7d62dedb350008c1af6cfb
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-02-12 16:10:36 +0700

    add new scripts fund_and_withdraw.

---
+++ scripts/fund_and_withdraw.py
@@ -0,0 +1,5 @@
+from brownie import FundMe
+from scripts.helpful_scripts import get_account
+def fund():
+    fund_me = FundMe[-1]
+    account = get_account()
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -3,18 +3,18 @@ from web3 import Web3 # type: ignore
 
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
-DECIMALS = 18
-STARTING_PRICE = 2000
+DECIMALS = 8
+STARTING_PRICE = 200000000000
 def get_account():
-    if network.show_active() == "development":
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
 
 def deploy_mocks():
         print(f"active network = {network.show_active()}")
-        print(f"Deploying mock...")
+        print(f"Deploying Mock...")
         if len(MockV3Aggregator) <= 0:
             MockV3Aggregator.deploy(
                 DECIMALS, Web3.to_wei(STARTING_PRICE, "ether"), {"from": get_account()})
-        print("Mock deployed")
+        print("Mock Deployed!")
