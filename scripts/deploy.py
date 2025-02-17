ff2ae9c0baface862fd41dab53451d90fdd501ea
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-02-18 01:05:41 +0700

    deploy ganache local

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,40 +1,29 @@
 from brownie import network, config, accounts, FundMe, MockV3Aggregator # type: ignore
-from scripts.helpful_scripts import get_account
+from scripts.helpful_scripts import get_account, deploy_mocks
 from dotenv import load_dotenv # type: ignore
 import os
 from web3 import Web3 # type: ignore
 
 load_dotenv()
 
-# Print out the token to verify
-print(f"ETHERSCAN_TOKEN: {os.getenv('ETHERSCAN_TOKEN')}")
-
 def deploy_fund_me():
-    # try:
+    try:
         account = get_account()
-        print(f"Deploying from account: {account}")
-        # pass the price feed address to our fundme contract
-
-        # if we are on a persistent network like rinkeby, use the associated address
-        # otherwise, deploy mocks
         if network.show_active() != "development":
             price_feed_address = config["networks"][network.show_active()][
                 "eth_usd_price_feed"
                 ]
         else:
-            print(f"The active network is {network.show_active()}")
-            print(f"Deploying Mocks...")
-            mock_aggregator = MockV3Aggregator.deploy(
-                18, 200000000000000000000000, {"from": account}
-                )
-            price_feed_address = mock_aggregator.address
-            print("Mocks Deployed!")
+            deploy_mocks()
+            price_feed_address = MockV3Aggregator[-1].address
 
         fund_me = FundMe.deploy(
             price_feed_address,
-            {"from": account}, publish_source=config["networks"][network.show_active()].get("verify"))
+            {"from": account, "gas_price": Web3.to_wei(30, "gwei"), "gas_limit": 5000000}, 
+            publish_source=config["networks"][network.show_active()].get("verify"))
         print(f"Contract deployed to {fund_me.address}")
-        
+    except Exception as e:
+        print(f"Error during deployment: {str(e)}")
         # if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         #     price_feed_address = config["networks"][network.show_active()][
         #         "eth_usd_price_feed"
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -3,8 +3,8 @@ from web3 import Web3 # type: ignore
 
 # LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
 
-# DECIMALS = 8
-# STARTING_PRICE = 200000000000
+DECIMALS = 8
+STARTING_PRICE = 200000000000
 
 def get_account():
     if network.show_active() == "development":
@@ -12,10 +12,10 @@ def get_account():
     else:
         return accounts.add(config["wallets"]["from_key"])
 
-# def deploy_mocks():
-#         print(f"active network = {network.show_active()}")
-#         print(f"Deploying Mock...")
-#         if len(MockV3Aggregator) <= 0:
-#             MockV3Aggregator.deploy(
-#                 DECIMALS, Web3.to_wei(STARTING_PRICE, "ether"), {"from": get_account()})
-#         print("Mock Deployed!")
+def deploy_mocks():
+        print(f"active network = {network.show_active()}")
+        print(f"Deploying Mock...")
+        if len(MockV3Aggregator) <= 0:
+            MockV3Aggregator.deploy(
+                DECIMALS,STARTING_PRICE, {"from": get_account(),"gas_price": Web3.to_wei(10, "gwei"), "gas_limit": 3000000})
+        print("Mock Deployed!")
