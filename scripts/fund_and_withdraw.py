49c56ac36c41cf6f6ea196d7145f604a6bad9114
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-04-24 12:12:36 +0700

    add fund function

--- scripts/fund_and_withdraw.py
+++ scripts/fund_and_withdraw.py
@@ -1,8 +1,13 @@
-from brownie import FundMe
+from brownie import FundMe, MockV3Aggregator, network, config
 from scripts.helpful_scripts import get_account
 def fund():
     fund_me = FundMe[-1]
     account = get_account()
     entrance_fee = fund_me.getEntranceFee()
-    print(entrance_fee)
+    print(f"The current entry fee is {entrance_fee}")
+    print("Funding...")
+    fund_me.fund({"from": account, "value": entrance_fee})
+    print("Funded!")
     
+def main():
+    fund()
