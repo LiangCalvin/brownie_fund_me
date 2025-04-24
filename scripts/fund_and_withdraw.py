d4f36d2418ff842fd2a2545ff82d12880216c12f
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-04-24 12:15:19 +0700

    add withdraw function

--- scripts/fund_and_withdraw.py
+++ scripts/fund_and_withdraw.py
@@ -9,5 +9,13 @@ def fund():
     fund_me.fund({"from": account, "value": entrance_fee})
     print("Funded!")
     
+def withdraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    print("Withdrawing...")
+    fund_me.withdraw({"from": account})
+    print("Withdrawn!")
+    
 def main():
     fund()
+    withdraw()
